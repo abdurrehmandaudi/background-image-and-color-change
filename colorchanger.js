08095b24169db6821147a024f55da0e8f963d002
shariq daudi <59463815+abdurrehmandaudi@users.noreply.github.com>
2020-11-21 19:59:37 +0530

    Add files via upload

---
+++ colorchanger.js
@@ -0,0 +1,209 @@
+                           // Image
+
+var wlp=document.getElementById("wrapper");
+
+// imga
+
+function back_imga(){
+    wlp.classList.value="";
+    wlp.classList.value="backimga"
+}
+document.getElementById("imga").addEventListener('click',back_imga);
+
+// imgb
+
+function back_imgb(){
+    wlp.classList.value="";
+    wlp.classList.value="backimgb"
+}
+document.getElementById("imgb").addEventListener('click',back_imgb);
+
+// imgb
+
+function back_imgb(){
+    wlp.classList.value="";
+    wlp.classList.value="backimgb"
+}
+document.getElementById("imgb").addEventListener('click',back_imgb);
+
+// imgc
+
+document.getElementById("imgc").addEventListener('click',function (){
+    wlp.classList.value="";
+    wlp.classList.value="backimgc";
+});
+
+// imgd
+
+document.getElementById("imgd").addEventListener('click',function (){
+    wlp.classList.value="";
+    wlp.classList.value="backimgd";
+});
+
+// imge
+
+document.getElementById("imge").addEventListener('click',function (){
+    wlp.classList.value="";
+    wlp.classList.value="backimge";
+});
+
+// imgf
+
+document.getElementById("imgf").addEventListener('click',function (){
+    wlp.classList.value="";
+    wlp.classList.value="backimgf";
+});
+
+// imgg
+
+document.getElementById("imgg").addEventListener('click',function (){
+    wlp.classList.value="";
+    wlp.classList.value="backimgg";
+});
+
+// imgh
+
+document.getElementById("imgh").addEventListener('click',function (){
+    wlp.classList.value="";
+    wlp.classList.value="backimgh";
+});
+
+// imgi
+
+document.getElementById("imgi").addEventListener('click',function (){
+    wlp.classList.value="";
+    wlp.classList.value="backimgi";
+});
+
+// imgj
+
+document.getElementById("imgj").addEventListener('click',function (){
+    wlp.classList.value="";
+    wlp.classList.value="backimgj";
+});
+
+
+                                // color
+
+var title=document.getElementById("title");
+var cont=document.getElementById("contentmain");
+
+  //pink
+
+function pink(){
+    title.classList="";
+    title.classList="pink";
+    cont.classList="";
+    cont.classList="pink";
+}
+document.getElementById("colora").addEventListener('click',pink);
+
+ //black
+
+function black(){
+    title.classList="";
+    title.classList="black";
+    cont.classList="";
+    cont.classList="black";
+}
+document.getElementById("colorb").addEventListener('click',black);
+
+//white
+
+function white(){
+   title.classList="";
+   title.classList="white";
+   cont.classList="";
+   cont.classList="white";
+}
+document.getElementById("colorc").addEventListener('click',white);
+
+//green
+
+function green(){
+   title.classList="";
+   title.classList="green";
+   cont.classList="";
+   cont.classList="green";
+}
+document.getElementById("colord").addEventListener('click',green);
+
+//red
+
+function red(){
+   title.classList="";
+   title.classList="red";
+   cont.classList="";
+   cont.classList="red";
+}
+document.getElementById("colore").addEventListener('click',red);
+
+//yellow
+
+function yellow(){
+   title.classList="";
+   title.classList="yellow";
+   cont.classList="";
+   cont.classList="yellow";
+}
+document.getElementById("colorf").addEventListener('click',yellow);
+
+//sky
+
+function sky(){
+   title.classList="";
+   title.classList="sky";
+   cont.classList="";
+   cont.classList="sky";
+}
+document.getElementById("colorg").addEventListener('click',sky);
+
+//purple
+
+function purple(){
+   title.classList="";
+   title.classList="purple";
+   cont.classList="";
+   cont.classList="purple";
+}
+document.getElementById("colorh").addEventListener('click',purple);
+
+//blue
+
+function blue(){
+   title.classList="";
+   title.classList="blue";
+   cont.classList="";
+   cont.classList="blue";
+}
+document.getElementById("colori").addEventListener('click',blue);
+
+//brown
+
+function brown(){
+   title.classList="";
+   title.classList="brown";
+   cont.classList="";
+   cont.classList="brown";
+}
+document.getElementById("colorj").addEventListener('click',brown);
+
+//grey
+
+function grey(){
+   title.classList="";
+   title.classList="grey";
+   cont.classList="";
+   cont.classList="grey";
+}
+document.getElementById("colork").addEventListener('click',grey);
+
+//golden
+
+function golden(){
+   title.classList="";
+   title.classList="golden";
+   cont.classList="";
+   cont.classList="golden";
+}
+document.getElementById("colorl").addEventListener('click',golden);
